7bd35273c08f4a2709761b7b194ef1cbffa31a5d
monica1302 <monica-bdd00@hotmail.com>
2023-08-30 17:35:43 -0600

    Actividad 3

--- p15-verifica-numero.py
+++ p15-verifica-numero.py
@@ -2,12 +2,13 @@
 
 print("Verificar si un numero entero es positivo, negativo o cero")
 numero = int(input("Dame un numero: "))
+
 if numero > 0 :
-print("El numero es positivo")
+    print("El numero es positivo")
 if numero < 0 :
-print("El numero es negativo")
+    print("El numero es negativo")
 if numero == 0 :
-print("El numero es cero")
+    print("El numero es cero")
 
 
 print("\n Proceso terminado")
