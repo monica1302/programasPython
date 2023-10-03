534f83c98dface354b7ec56b75e817e7877aa9e1
monica1302 <monica-bdd00@hotmail.com>
2023-10-02 22:01:32 -0600

    Tarea 4

--- p59-secuencia-terminos-1.py
+++ p59-secuencia-terminos-1.py
@@ -5,15 +5,14 @@ f = 1
 s=0
 
 for i in range(1,n+1):
+    print(f"1/{i}! + ", end="")
     f=1
+    
     for j in range(1,i+1):
         f =f * j
     f=1/f
     s+=f
 
-for i in range(1,n+1):
-    print(f"1/{i}! + ", end="")
     if i==n:
         print (f"= {s:.3f}")
-
 print("\nProceso terminado")
